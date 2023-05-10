f50df387ca2c7f473b6c3deb8918bce36d37a296
Helmi <helmiariefm@gmail.com>
2023-05-10 19:34:34 +0700

    Endpoint - Detail Pasien

--- app.js
+++ app.js
@@ -15,4 +15,19 @@ app.get('/pasien-list', async (req, res) => {
     }
 })
 
+app.get('/pasien-detail/:id', async (req, res) => {
+    const {id} = req.params
+
+    try {
+        const getPasienById = await pasien.findByPk(id)
+        if(getPasienById){
+            res.status(200).json(getPasienById)
+        }else{
+            res.status(404).json({message: 'Pasien Not Found'})
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 app.listen(port, () => console.log(`Running on ${port}`))
