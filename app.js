ba61240551071d8a9ef76f0ebb9ace5c2265986c
Helmi <helmiariefm@gmail.com>
2023-05-10 19:28:16 +0700

    Endpoint - List Pasien

--- app.js
+++ app.js
@@ -6,4 +6,13 @@ const {pasien} = require('./models')
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
+app.get('/pasien-list', async (req, res) => {
+    try {
+        const data = await pasien.findAll()
+        res.status(200).json(data)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 app.listen(port, () => console.log(`Running on ${port}`))
