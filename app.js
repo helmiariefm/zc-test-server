9f9d3949b0663c219036b971b27e0c9cbf86166e
Helmi <helmiariefm@gmail.com>
2023-05-18 10:08:06 +0700

    setting up supabase and railway deploy

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config() 
+}
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
+const cors = require('cors')
 const {pasien} = require('./models')
 
+app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 app.get('/pasien-list', async (req, res) => {
     try {
-        const data = await pasien.findll()
+        const data = await pasien.findAll()
         res.status(200).json(data)
     } catch (err) {
+        console.log(err)
         res.status(500).json(err)
     }
 })
